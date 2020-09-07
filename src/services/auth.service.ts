65badca48c73c3fdbd8d88131d17bd2d2636446a
Emerson Melo <emerson_sardinha@hotmail.com>
2020-09-07 12:48:34 -0300

    Criando uma pagina de profile

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../config/api.config";
 import { LocalUser } from "../models/local_user";
 import { StorageService } from "./storage.service";
-import { JwtHelper } from "angular2-jwt";
+import { JwtHelper } from 'angular2-jwt';
 
 
 //aula 121
